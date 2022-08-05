fb8f7f4f724f447ced3ba95561813e2bbebb58cb
Md. Soliman Ali <soliman15-8572@diu.edu.bd>
2022-08-05 13:47:13 +0600

    Add files via upload

--- cypress/e2e/order.cy.js
+++ cypress/e2e/order.cy.js
@@ -21,7 +21,7 @@ describe('Order Product', () => {
         cy.get(".product-container").eq(0).click();
     })
     it('Confirm Order', () => {
-        cy.get("img").eq(13).click();
+        cy.get("img").eq(13).click();   // Define Array
         cy.get("#add_to_cart").click();
         cy.wait(5000);
         cy.xpath("//span[normalize-space()='Proceed to checkout']").click();
